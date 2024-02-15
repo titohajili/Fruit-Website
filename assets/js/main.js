7af9aff9e2bb8cceefb9313caab5b260dc8ef9e3
titohajili <maqatoto963@gmail.com>
2024-02-16 02:00:08 +0400

    Shadow Header & Enjoy Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,8 +27,15 @@ const linkAction = () => {
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
+//shadow header
 
+const shadowHeader = () => {
+    const header = document.getElementById('header')
 
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
 
 
 
