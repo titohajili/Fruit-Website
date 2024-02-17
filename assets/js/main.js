6c46c333d5ec8b693b17658ef93ce4758ed72b3f
titohajili <maqatoto963@gmail.com>
2024-02-17 20:50:40 +0400

    Scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,5 +37,14 @@ const shadowHeader = () => {
 }
 window.addEventListener('scroll', shadowHeader)
 
+//SCROLL-UP=====
 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove("show-scroll")
+}
+
+window.addEventListener('scroll', scrollUp)
 
