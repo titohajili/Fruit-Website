5db4cca309c5313b80e2e1d1a2e834b7040a60a0
titohajili <maqatoto963@gmail.com>
2024-02-19 01:57:55 +0400

    ScrollRevealAnimation

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,3 +100,20 @@ themeButton.addEventListener('click', () =>{
     localStorage.setItem('selected-icon', getCurrentIcon())
 
 })
+
+//scroll reveal animation
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true,//Animation repeat
+})
+
+sr.reveal(`.home__data , .join__container, .footer`)
+sr.reveal(`.home__img`, {origin: 'bottom'})
+sr.reveal(`.enjoy__card , .popular__card`, {interval: 100})
+sr.reveal(`.about__data`, {origin: 'right'})
+sr.reveal(`.about__img`, {origin: 'left'})
+
